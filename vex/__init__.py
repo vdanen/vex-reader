0db3ac94e4c3d63eb0285fbc37535da150fe9052
Vincent Danen <vdanen@redhat.com>
2024-08-04 19:43:05 -0600

    more renames and we'll call this 0.2

--- vex/__init__.py
+++ vex/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2024 Vincent Danen
 # License: GPLv3+
 
-from .vex1 import Vex
+from .vex import Vex
 from .package import VexPackages
 from .simplenvd import NVD
 
-__version__ = '0.1'
+__version__ = '0.2'
