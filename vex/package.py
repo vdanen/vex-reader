31f96b5459d3d3bbdef17e27b5ae36e85006369d
Aubrey Olandt <aubreylolandt@gmail.com>
2026-02-10 17:44:23 -0800

    Fixed changes

--- vex/package.py
+++ vex/package.py
@@ -210,24 +210,7 @@ class VexPackages(object):
                 continue
             # TODO there seems to be a bug in the VEX output respective to branch nesting, it's very convoluted =(
             for b in p['branches']:
-                # Check if b itself is a product_name (MSRC structure: vendor → product_name → product_version)
-                if 'category' in b and b['category'] == 'product_name':
-                    name = b['name']
-                    if 'branches' in b:
-                        for c in b['branches']:
-                            # c should have product_version or product_version_range with a product object
-                            if 'product' in c:
-                                cpe  = None
-                                purl = None
-                                id  = c['product']['product_id']
-                                if 'product_identification_helper' in c['product']:
-                                    if 'cpe' in c['product']['product_identification_helper']:
-                                        cpe = c['product']['product_identification_helper']['cpe']
-                                    if 'purl' in c['product']['product_identification_helper']:
-                                        purl = c['product']['product_identification_helper']['purl']
-                                self.pmap.append({id: {'name': name, 'cpe': cpe, 'purl': purl}})
-                # Original logic for Red Hat structure: vendor → product_family → product_name
-                elif 'branches' in b.keys():
+                if 'branches' in b.keys():
                     for c in b['branches']:
                         if 'category' in c.keys():
                             if c['category'] == 'product_name':
@@ -253,29 +236,20 @@ class VexPackages(object):
                                         if 'purl' in c['product']['product_identification_helper']:
                                             purl = c['product']['product_identification_helper']['purl']
                                 self.pmap.append({id: {'name': name, 'cpe': cpe, 'purl': purl}})
-                elif 'category' in b.keys() and 'product' in b.keys():
-                    # Handle branches that have product directly (no nested branches)
-                    # This handles both product_name and product_version categories
+                elif 'product' in b and 'category' in b:
+                    # Handle products at Level 1 that don't have nested branches
+                    # These are products themselves (category: product_name or product_version)
                     if b['category'] in ['product_name', 'product_version']:
                         name = b['name']
                         cpe  = None
                         purl = None
-                        id  = b['product']['product_id']
+                        id   = b['product']['product_id']
                         if 'product_identification_helper' in b['product']:
                             if 'cpe' in b['product']['product_identification_helper']:
                                 cpe = b['product']['product_identification_helper']['cpe']
                             if 'purl' in b['product']['product_identification_helper']:
                                 purl = b['product']['product_identification_helper']['purl']
                         self.pmap.append({id: {'name': name, 'cpe': cpe, 'purl': purl}})
-
-        # Parse relationships to get composite product IDs (used by both Red Hat and MSRC)
-        if 'relationships' in self.raw['product_tree']:
-            for rel in self.raw['product_tree']['relationships']:
-                if 'full_product_name' in rel:
-                    fpn = rel['full_product_name']
-                    if 'product_id' in fpn and 'name' in fpn:
-                        # Relationships don't typically have CPE/PURL, set to None
-                        self.pmap.append({fpn['product_id']: {'name': fpn['name'], 'cpe': None, 'purl': None}})
 
 
     def parse_packages(self):
@@ -287,25 +261,9 @@ class VexPackages(object):
         self.not_affected = []
 
         for k in self.raw['vulnerabilities']:
-            # Determine VEX file format: Check if vendor_fix product_ids match product_status.fixed
-            # Red Hat: vendor_fix product_ids ARE in product_status.fixed (use remediations)
-            # MSRC: vendor_fix product_ids are NOT in product_status.fixed (use product_status.fixed instead)
-            use_remediations = True
-            if 'remediations' in k and 'product_status' in k and 'fixed' in k['product_status']:
-                # Check if any vendor_fix product_id is in product_status.fixed
-                fixed_set = set(k['product_status']['fixed'])
-                vendor_fix_ids = []
-                for r in k['remediations']:
-                    if r['category'] == 'vendor_fix':
-                        vendor_fix_ids.extend(r['product_ids'])
-
-                # If vendor_fix product_ids don't overlap with fixed, use product_status.fixed instead
-                if vendor_fix_ids and not any(pid in fixed_set for pid in vendor_fix_ids):
-                    use_remediations = False
-
             if 'remediations' in k:
                 for x in k['remediations']:
-                    if x['category'] == 'vendor_fix' and use_remediations:
+                    if x['category'] == 'vendor_fix':
                         # TODO: the assumption here is that there is one product, and potentially many components
                         # which doesn't seem to be the case, see https://www.sick.com/.well-known/csaf/white/2024/sca-2024-0001.json
                         # which has two vendor_fix statements, but the second has more than one product_ids; this will
@@ -322,34 +280,8 @@ class VexPackages(object):
                         for y in (x['product_ids']):
                             self.wontfix.append(WontFix(y, x, self.pmap))
 
-            # Process product_status section
             if 'product_status' in k:
                 for x in k['product_status']:
-                    # Only process 'fixed' when NOT using remediations (MSRC-style)
-                    if x == 'fixed' and not use_remediations:
-                        # MSRC-style VEX: list fixed products from product_status.fixed
-                        # Try to find associated remediation info (URL, date) from vendor_fix remediations
-                        remediation_info = None
-                        if 'remediations' in k:
-                            for r in k['remediations']:
-                                if r['category'] == 'vendor_fix':
-                                    remediation_info = r
-                                    break
-
-                        # Create Fix objects for each fixed product
-                        for y in filter_components(k['product_status']['fixed']):
-                            # Build a remediation-like dict for Fix class
-                            fix_data = {'product_ids': [y]}
-                            if remediation_info:
-                                if 'url' in remediation_info:
-                                    fix_data['url'] = remediation_info['url']
-                                if 'date' in remediation_info:
-                                    fix_data['date'] = remediation_info['date']
-                                if 'details' in remediation_info:
-                                    fix_data['details'] = remediation_info['details']
-                            self.fixes.append(Fix(fix_data, self.pmap))
-
-                    # Always process known_affected and known_not_affected (for all VEX styles)
                     if x == 'known_affected':
                         for y in filter_components(k['product_status']['known_affected']):
                             self.affected.append(Affected(y, self.pmap))
